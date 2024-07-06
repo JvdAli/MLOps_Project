b462d8e76e43b8b3687806708f6eb8a93735240d
Javed <jvdali.e@gmail.com>
2024-07-06 14:27:26 +0400

    updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -28,15 +28,15 @@ class DataIngestion:
 
             #Creating directory to store raw and splitted datasets
             os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True) #creating artifacts folder
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) # saving raw dataset in csv folder in dataframe format
+            df.to_csv(self.ingestion_config.raw_data_path,index=False, header=True) # saving raw dataset in csv folder in dataframe format
 
             #splitting raw date into train & test
             train_set, test_set = train_test_split(df, test_size=.2 ,random_state=42)
             logging.info("Dataset splitted successfully")
             
             #Saving splitted data 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False, header=True)
             logging.info("splitted dataset saved successfully")
 
             #returing files path
